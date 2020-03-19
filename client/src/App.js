2c1c55587c6b7af89f99b6e2aae33a1d8ac33f67
Joseph Yannessa <baconcheese113@hotmail.com>
2020-03-18 20:36:47 -0600

    Sample file for testing auto-fixing hook rules

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App(props) {
+  const { p } = props
+
+  // TODO this hook should automatically add dependencies
+  React.useEffect(() => {
+    return () => {
+      if (p && p.p) {
+        p.p = 1
+      }
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +21,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
